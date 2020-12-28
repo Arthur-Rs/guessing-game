c57e7725842b753176d66c26f3617fafecddc80a
Arthur-Rs <arthurreis074@gmail.com>
2020-12-28 20:51:18 -0300

    Update: Added validation

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,24 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io::{self, Write};
 
+pub struct Palpite {
+    valor: u8
+}
+
+impl Palpite {
+    pub fn new(valor: u8) -> Palpite {
+        if valor < 1 || valor > 100 {
+            panic!("O número precisa estar entre 1 e 100");
+        }
+
+        Palpite { valor }
+    }
+
+    pub fn valor(&self) -> u8 {
+        self.valor
+    }
+}
+
 fn main() {
     println!(
         "{}",
@@ -11,29 +29,31 @@ fn main() {
             .paint("Bem-vido ao jogo de adivinhação!")
     );
 
-    let secret_number: u8 = rand::thread_rng().gen_range(0..100);
+    let numero_secreto: u8 = rand::thread_rng().gen_range(0..100);
 
     loop {
         print!("Selecione um número: ");
 
         io::stdout().flush().unwrap();
 
-        let mut number = String::new();
+        let mut palpite = String::new();
 
-        match io::stdin().read_line(&mut number) {
+        match io::stdin().read_line(&mut palpite) {
             Ok(_) => (),
             Err(_) => println!("Não foi possível lé o número!"),
         };
 
-        let number = match number.trim().parse::<u8>() {
-            Ok(number) => number,
-            Err(number) => {
-                println!("{} não é um número!", number);
+        let palpite = match palpite.trim().parse::<u8>() {
+            Ok(palpite) => palpite,
+            Err(palpite) => {
+                println!("{} não é um número!", palpite);
                 continue;
             }
         };
 
-        match number.cmp(&secret_number) {
+        let palpite = Palpite::new(palpite);
+
+        match palpite.valor().cmp(&numero_secreto) {
             Ordering::Less => println!("{}", Colour::Red.paint("Tente um número maior!")),
             Ordering::Greater => println!("{}", Colour::Red.paint("Tente um número menor!")),
             Ordering::Equal => {
